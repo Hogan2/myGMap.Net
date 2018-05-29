f29d0d95314afb7da630ff6672bf4a654ceaea40
Hogan2 <loggg2333@gmail.com>
2018-05-29 09:45:33 +0800

    00011

---
+++ Demo/Demo.WindowsPresentation/Demo.WindowsPresentation/Bootstrapper.cs
@@ -0,0 +1,26 @@
+﻿using Demo.WindowsPresentation.Views;
+using MapTest;
+using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Unity;
+using System.Windows;
+
+namespace Demo.WindowsPresentation
+{
+    class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+        protected override void ConfigureModuleCatalog()
+        {
+            var catalog = (ModuleCatalog)ModuleCatalog;
+            catalog.AddModule(typeof(MapTestModule));
+        }
+    }
+}
---
+++ Testing/MapTest/MapTest/MapTestModule.cs
@@ -0,0 +1,25 @@
+﻿using MapTest.Views;
+using Prism.Modularity;
+using Prism.Regions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapTest
+{
+    public class MapTestModule : IModule
+    {
+        IRegionManager _regionManager;
+        public MapTestModule(RegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MapRegion", typeof(MapTestView));
+        }
+    }
+}
